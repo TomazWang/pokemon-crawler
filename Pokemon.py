26ea1a43bb9f93eefc5f4c217b5125e6f9e578dc
Tomaz Wang <tomazwang.tw@gmail.com>
2016-08-29 15:57:39 +0800

    Add some comment for note

--- Pokemon.py
+++
@@ -1,9 +0,0 @@
-class Pokemon:
-
-    def __init__(self, id, name_zh, name_en, link, pic_link):
-        self.id = id
-        self.name_zh = name_zh
-        self.name_en = name_en
-        self.link = link
-        self.pic_link = pic_link
-
